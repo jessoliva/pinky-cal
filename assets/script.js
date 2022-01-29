bd5bb3223c1f050e0525cd068f615a79b995fc6b
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-01-29 17:49:05 -0600

    JS: commented out code

--- assets/script.js
+++ assets/script.js
@@ -33,17 +33,48 @@
 // 1. When I refresh the page, the property persists
 // On refresh, grab the data from local storage and display it in the appropriate time block
 
+
+
+function currDate() {
+    // set current day to current day
+    let currentDay = $('#currentDay');
+    currentDay.text(moment().format('dddd, MMMM Do YYYY'));
+
+    setInterval (function() {
+        // set current day to current day
+        let currentTime = $('#currentTime');
+        currentTime.text(moment().format('h:mm:ss A'));
+
+    }, 1000);
+}
+currDate();
+
+// reference the container element
+const containerEl = $('.container');
+
 // dynamically create the timeblocks
 function createBlocks() {
 
+    // let time00 = [moment('7:00 AM', 'h:mm A').format('h:mm A'), '800 AM', '900 AM', '1000 AM', '1100 AM', '1200 PM', '100 PM', '200 PM', '300 PM', '400 AM', '500 PM'];
+    // let time30 = ['730 AM', '830 AM', '930 AM', '1030 AM', '1130 AM', '1230 PM', '130 PM', '230 PM', '330 PM', '430 AM', '530 PM'];
+
+    let time00 = ['700 AM', '800 AM', '900 AM', '1000 AM', '1100 AM', '1200 PM', '100 PM', '200 PM', '300 PM', '400 AM', '500 PM'];
+    let time30 = ['730 AM', '830 AM', '930 AM', '1030 AM', '1130 AM', '1230 PM', '130 PM', '230 PM', '330 PM', '430 AM', '530 PM'];
+
+    // console.log(time00[0]);
+
+    // let timeNow = moment().format('h:mm A');
+    // console.log(timeNow);
+
+    // let sevenAm = moment('7:00 AM', 'h:mm A');
+    // console.log(sevenAm);
+
+    // let timeNowN = moment();
+    // console.log(timeNowN);
+
+    // console.log(sevenAm.isBefore(timeNowN));
+
     for(i = 0; i < 11; i++) {
-
-        let time00 = ['700 AM', '800 AM', '900 AM', '1000 AM', '1100 AM', '1200 PM', '100 PM', '200 PM', '300 PM', '400 AM', '500 PM'];
-        let time30 = ['730 AM', '830 AM', '930 AM', '1030 AM', '1130 AM', '1230 PM', '130 PM', '230 PM', '330 PM', '430 AM', '530 PM'];
-
-        // reference the container element
-        const containerEl = $('.container');
-
         // create the row div element
         const rowEl = $("<div>").addClass("row d-flex justify-content-center");
 
@@ -58,9 +89,9 @@ function createBlocks() {
 
         // create description element
         const desEl = $("<div>").addClass("description col-8 p-0 d-flex flex-column");
-        // create 00 time element
+        // create 00 des element
         let des1El = $("<textarea>").addClass("dtop h-50");
-        // create 30 time element
+        // create 30 des element
         let des2El = $("<textarea>").addClass("dbot h-50");
         // append des1 and des2 to description
         desEl.append(des1El, des2El)
@@ -76,9 +107,66 @@ function createBlocks() {
         rowEl.append(hourEl, desEl, btnEl);
 
         // append row to container
-        containerEl.append(rowEl)
+        containerEl.append(rowEl);
     }
+
+    // $('.htop').each(function () {
+
+    //     // convert to military time and compare those
+    //     // for each method to loop through array of time and convert to military time ALSO .map
+
+
+    //     let timeNowInt = parseInt(timeNow); // 12
+    //     // console.log("timenow is " + timeNowInt);
+
+    //     let intTime00 = parseInt(time00[0]); // 7
+    //     // console.log('time past is ' + intTime00)
+
+    //     console.log(timeNowInt, ' vs ', intTime00);
+
+    //         // 7          12
+    //     if (intTime00 < timeNow) {
+    //         console.log('past');
+    //     }
+    //     if (intTime00 > timeNow) {
+    //         console.log('hi');
+    //     }
+
+    // });
 };
 createBlocks();
 
-// when the 
+
+
+
+
+
+
+
+
+// // when button is clicked, convert textarea to one text area
+// $('.saveBtn').click(function() {
+
+//     // find description div
+//     const des = $(this).closest('div').find('.description');
+//     // remove children
+//     des.remove();
+
+//     // create 1 textarea
+//     let textEl = $("<textarea>").addClass("dtop h-100");
+
+//     // append to div
+//     des.append(textEl);
+
+//     containerEl.append(des);
+
+//     console.log(des);
+
+// });
+
+// function timeBlockColor() {
+
+//     let currTime = 
+
+// }
+
